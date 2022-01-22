bd1735e052c6a4d29f9e0630c93e90d203ea4dcb
bhasi <anilbhasib@gmail.com>
2022-01-22 14:55:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.scss"
 
 function App() {
+  const [switchState1, setSwitchState1] = useState(false)
+  const [switchState2, setSwitchState2] = useState(false)
+  const handleSwitchState1 = () => {
+    setSwitchState1(!switchState1)
+  }
+  const handleSwitchState2 = () => {
+    setSwitchState2(!switchState2)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="bulbs">
+          <div
+            className="bulb"
+            style={
+              switchState1
+                ? { backgroundColor: "orange" }
+                : { backgroundColor: "grey" }
+            }
+          ></div>
+          <div
+            className="bulb"
+            style={
+              switchState2
+                ? { backgroundColor: "orange" }
+                : { backgroundColor: "grey" }
+            }
+          ></div>
+        </div>
+        <div className="buttons">
+          <Button
+            switchState={switchState1}
+            handleSwitchState={handleSwitchState1}
+          />
+          <Button
+            switchState={switchState2}
+            handleSwitchState={handleSwitchState2}
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+function Button({ switchState, handleSwitchState }) {
+  return (
+    <button onClick={() => handleSwitchState()}>
+      {" "}
+      {switchState ? "Off" : "On"}
+    </button>
+  )
+}
